d096e1964b34b88ce6d6a32ed1591566b89f2210
braisv <brais.vidal.rodriguez@gmail.com>
2020-01-26 15:20:08 +0100

    added some styles

--- client/src/Components/User/User.jsx
+++ client/src/Components/User/User.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./User.scss";
 
 
 const User = () => {
     return (
         <div>
-            <div className="user-options">
+            <div className="user-options flex">
                 <div className="user-option">New here</div>
                 <div className="user-option">Already user</div>
             </div>
